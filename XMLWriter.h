edf80901062a207a9900926e915bcccfb1d27d42
ahmyi <cryptoamity@gmail.com>
2011-11-09 18:56:34 +0800

    Cleaner Code

--- XMLWriter.h
+++ XMLWriter.h
@@ -6,33 +6,10 @@
 
 @interface XMLWriter : NSObject{
 @private
-    int treeLevel ,nodeCounts, node, objectlevel, currentNode, samenodeflag;
     NSMutableArray* nodes;
     NSString* xml;
-    NSMutableArray* oldroot;
     NSMutableArray* treeNodes;
 }
-@property (nonatomic)int treeLevel, node, objectlevel, nodeCounts, currentNode, samenodeflag;
-@property (nonatomic, retain)NSMutableArray* nodes;
-@property (nonatomic, retain)NSString* xml;
-@property (nonatomic, retain)NSMutableArray* oldroot;
-@property (nonatomic, retain)NSMutableArray* treeNodes;
-
-
--(void)serialize:(id)root;
--(void)pushRootHistory:(id)root;
--(id)callOldRoot;
--(void)checkParent;
--(void)push:(NSString *)stack;
--(void)pop;
-
--(NSString *)xmlString;
--(NSString *)endString;
--(void)Stack:(NSString *)tag;
--(void)pushStackValue:(NSString *)stack;
--(void)pushStackValueArray:(NSString *)stack;
-
-
 +(NSString *)XMLStringFromDictionary:(NSDictionary *)dictionary;
 +(BOOL)XMLDataFromDictionary:(NSDictionary *)dictionary toStringPath:(NSString *) path  Error:(NSError **)error;
 
--- XMLWriter.m
+++ XMLWriter.m
@@ -4,179 +4,92 @@
 #import "XMLWriter.h"
 
 @implementation XMLWriter
-@synthesize treeLevel,node, objectlevel,nodeCounts, currentNode, nodes, xml, oldroot, samenodeflag, treeNodes;
-- (id)init
+- (id)initWithDictionary:(NSDictionary *)dictionary
 {
     self = [super init];
     if (self) {
         // Initialization code here.
-        self.currentNode = -1;
-        self.treeLevel = 0;
-        self.objectlevel = 0;
-        self.node = 0;
-        self.xml = @"<?xml version="1.0" ?>";
-        self.nodeCounts = 0;
-        self.samenodeflag = 0;
+        xml = @"<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
         nodes = [[NSMutableArray alloc] init]; 
-        oldroot = [[NSMutableArray alloc] init]; 
         treeNodes = [[NSMutableArray alloc] init]; 
-
+        [self serialize:dictionary];
     }
     
     return self;
 }
 -(void)dealloc
 {
+    [xml release],nodes =nil;
     [nodes release], nodes = nil ;
-    [oldroot release], oldroot = nil;
     [treeNodes release], treeNodes = nil;
     [super dealloc];
 }
+
+-(void)serialize:(id)root
+{    
+    if([root isKindOfClass:[NSArray class]])
+        {
+            int mula = (int)[root count];
+            mula--;
+            [nodes addObject:[NSString stringWithFormat:@"%i",(int)mula]];
+
+            for(id objects in root)
+            {
+                if ([[nodes lastObject] isEqualToString:@"0"] || [nodes lastObject] == NULL || ![nodes count])
+                {
+                    [nodes removeLastObject];
+                    [self serialize:objects];
+                }
+                else
+                {
+                    [self serialize:objects];
+                    xml = [xml stringByAppendingFormat:@"</%@><%@>",[treeNodes lastObject],[treeNodes lastObject]];
+                    int value = [[nodes lastObject] intValue];
+                    [nodes removeLastObject];
+                    value--;
+                    [nodes addObject:[NSString stringWithFormat:@"%i",(int)value]];
+                }
+            }
+        }
+        else if ([root isKindOfClass:[NSDictionary class]])
+        {
+            for (NSString* key in root)
+            {
+                [treeNodes addObject:key];
+                xml = [xml stringByAppendingFormat:@"<%@>",key];
+                [self serialize:[root objectForKey:key]];
+                xml =[xml stringByAppendingFormat:@"</%@>",key];
+                [treeNodes removeLastObject];
+            }
+        }
+        else if ([root isKindOfClass:[NSString class]])
+        {
+            xml = [xml stringByAppendingFormat:@"%@",root];
+        }
+}
+-(NSString *)getXML
+{
+    return xml;
+}
+
+
+
 +(NSString *)XMLStringFromDictionary:(NSDictionary *)dictionary
 {
     if (![[dictionary allKeys] count])
         return NULL;
-    
-    XMLWriter* fromDictionary = [[[XMLWriter alloc]init]autorelease];
-    [fromDictionary serialize:dictionary];
-    return [fromDictionary xmlString];
+    XMLWriter* fromDictionary = [[[XMLWriter alloc]initWithDictionary:dictionary]autorelease];
+    return [fromDictionary getXML];
 }
 +(BOOL)XMLDataFromDictionary:(NSDictionary *)dictionary toStringPath:(NSString *) path  Error:(NSError **)error
 {
     
-    XMLWriter* fromDictionary = [[[XMLWriter alloc]init]autorelease];
-    [fromDictionary serialize:dictionary];
-    [[fromDictionary xmlString] writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:error];
-    return YES;
-
-}
-
--(void)serialize:(id)root
-{
-    [self pushRootHistory:root];
+    XMLWriter* fromDictionary = [[[XMLWriter alloc]initWithDictionary:dictionary]autorelease];
+    [[fromDictionary getXML] writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:error];
+    if (error)
+        return FALSE;
+    else
+        return TRUE;
     
-    if([root isKindOfClass:[NSArray class]])
-        {
-            [treeNodes addObject:root];
-
-            for (int i = 0;i <[root count];i++)
-            {
-                if([[root objectAtIndex:i] isKindOfClass:[NSString class]])
-                [self pushStackValue:[root objectAtIndex:i]];
-                    else
-                [self serialize:[root objectAtIndex:i]];
-                nodeCounts++;
-            }
-            [treeNodes removeLastObject];
-
-
-        }
-        else if ([root isKindOfClass:[NSDictionary class]])
-        {
-            NSArray* trunk = [root allKeys];
-            currentNode++;
-            NSString* storeTag;
-            for(int i =0;i<[trunk count];i++){
-                treeLevel = currentNode;
-                storeTag = [trunk objectAtIndex:i];
-                [self Stack:storeTag];
-                [self serialize:[root objectForKey:[trunk objectAtIndex:i ]]];
-            }
-            currentNode--;
-        }
-        else if ([root isKindOfClass:[NSString class]])
-        {
-            objectlevel ++;
-            treeLevel = (objectlevel+currentNode);
-                [self pushStackValue:root];
-
-            objectlevel --; //string end
-        }
-        
-}
--(void)pushRootHistory:(id)root
-{
-    if(![oldroot count])
-        [oldroot removeLastObject];
-    [oldroot addObject:root];
-}
--(id)callOldRoot
-{
-    return [oldroot objectAtIndex:0];
-}
--(NSString *)nodeIndex
-{
-    return [nodes objectAtIndex:(node-1)];
-}
--(void)push:(NSString *)stack
-{
-    [nodes addObject:stack];
-    node++; 
-    
-}
--(void)pop
-{
-//    xml = [xml stringByAppendingFormat:@"</%@>",[self nodeIndex]];                
-    node--;
-    [nodes removeLastObject];
-}
--(void)Stack:(NSString *)stack
-{
-
-    for (int i = treeLevel; i < [nodes count]; i++) {
-        
-        [self pop];
-
-    } 
-    [self checkParent];    
-//    NSLog(@" <> stack => %@",stack);
-
-    [self push:stack];
-    xml = [xml stringByAppendingFormat:@"<%@>",stack];
-
-   
-}
--(void)checkParent
-{
-    if (0<nodeCounts)
-    {   
-//        NSLog(@"%d ====> %@",nodeCounts,[self nodeIndex]);
-        xml = [xml stringByAppendingFormat:@"</%@>",[self nodeIndex]];                
-//        xml = [xml stringByAppendingFormat:@"<%@>",[self nodeIndex]];
-        nodeCounts--;
-    }
-}
--(void)pushStackValue:(NSString *)stack
-{
-    [self push:stack];
-//    NSLog(@" Ω stack => %@",stack);
-
-    xml = [xml stringByAppendingFormat:@"%@",stack];
-    node--;
-    [nodes removeLastObject];
-    xml = [xml stringByAppendingFormat:@"</%@>",[self nodeIndex]];                
-
-    
-}
--(NSString *)endString
-{
-
-    node--;
-    [nodes removeLastObject];
-
-    do{
-        node--;
-        xml = [xml stringByAppendingFormat:@"</%@>",[nodes objectAtIndex:node]]; 
-        [nodes removeLastObject];
-
-    }while (node != 0);
-    
-
-    return self.xml;
-}
-
--(NSString *)xmlString
-{
-    return [self endString];
 }
 @end
